590f27c9376b0e7743fb90533ab95db76a5a62a5
John Watson <jwatson3d@gmail.com>
2022-02-06 12:33:11 -0600

    add infra/bootstrap- stand up codepipeline+website

---
+++ infra/bootstrap.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# generate random identifier
+RANDOM_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
+
+# obtain current user name
+OWNER_NAME=$(aws sts get-caller-identity | jq -r ".Arn" | cut -d'/' -f2)
+USER_NAME=$(sed -e "s/_/-/g" <<<$OWNER_NAME)
+
+# create a unique stack name
+STACK_NAME=$USER_NAME-$RANDOM_ID
+
+# obtain source repository information (https://github.com/james-turner/github-to-ci-in-5-minutes/blob/master/bootstrap.sh)
+#SOURCE_TYPE=$(git remote -v | grep push | cut -d ':' -f1 | cut -d '@' -f2 | cut -d '.' -f1)
+#SOURCE_TYPE=$(tr '[:lower:]' '[:upper:]' <<< ${SOURCE_TYPE:0:1})${SOURCE_TYPE:1}
+# above two lines generate 'Github' while codestar-connections requires 'GitHub'
+SOURCE_TYPE=GitHub
+CREDENTIALS_ARN=$(aws codestar-connections list-connections --provider-type-filter $SOURCE_TYPE --max-results 10 --query "Connections[?ConnectionStatus=='AVAILABLE']|[0].ConnectionArn" --output text)
+BRANCH=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+PROJECT_NAME=$(basename `pwd`)
+REPOSITORY_OWNER=$(git remote -v | grep push | cut -d ':' -f2 | cut -d '/' -f1)
+REPOSITORY_ID=$REPOSITORY_OWNER/$PROJECT_NAME
+
+# create the stack using these random names
+echo "Creating CloudFormation stack $STACK_NAME"
+STACK_ID=$(aws cloudformation create-stack \
+  --stack-name "$STACK_NAME" \
+  --template-body file://bootstrap.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=BranchName,ParameterValue=$BRANCH \
+      ParameterKey=CredentialsArn,ParameterValue=$CREDENTIALS_ARN \
+      ParameterKey=CurrentUserName,ParameterValue=$OWNER_NAME \
+      ParameterKey=ProjectName,ParameterValue=$PROJECT_NAME \
+      ParameterKey=RandomId,ParameterValue=$RANDOM_ID \
+      ParameterKey=RepositoryId,ParameterValue=$REPOSITORY_ID &)
+
+# obtain the stack ID just created
+STACK_ARN=$(jq -r '.StackId' <<<$STACK_ID)
+
+echo "Waiting for STACK_ARN ${STACK_ARN} to be completed"
+aws cloudformation wait stack-create-complete --stack-name "${STACK_ARN}"
+
+echo "Stack ${STACK_NAME} has been created:"
+STACK_INFO=$(aws cloudformation describe-stacks --stack-name $STACK_NAME)
+
+BUCKET_NAME=$(jq -r '.Stacks[0].Outputs[0].OutputValue' <<<$STACK_INFO)
+echo "S3 Bucket '${BUCKET_NAME}' has been created"
+
+KEY_ID=$(jq -r '.Stacks[0].Outputs[1].OutputValue' <<<$STACK_INFO)
+KEY_ALIAS=$(jq -r '.Stacks[0].Outputs[2].OutputValue' <<<$STACK_INFO)
+echo "Key '${KEY_ID}' with alias '${KEY_ALIAS}' has been created"
+
+export KEY_ID
+export KEY_ALIAS
+export BUCKET_NAME
+
+# pause to allow inspection
+echo "Press any key to continue"
+while [ true ] ; do
+  read -t 5 -n 1
+  if [ $? = 0 ] ; then
+    break ;
+  else
+    echo "waiting for the keypress"
+  fi
+done
+
+# remove the stack
+#aws cloudformation delete-stack --stack-name "$STACK_NAME"
